c0acef39216b37d2f1f7eeb3fc32a1e344dda7c3
Micalhl <micalhl@sina.cn>
2023-01-30 09:31:35 +0800

    Change To Gradle

---
+++ src/main/kotlin/com/mcstarrysky/floatingmarket/FloatingMarket.kt
@@ -0,0 +1,11 @@
+package com.mcstarrysky.floatingmarket
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object FloatingMarket: Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
